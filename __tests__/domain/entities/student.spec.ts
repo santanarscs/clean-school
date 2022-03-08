d0e5093774705494ceb0a12c7662d717797d8765
santanarscs <raphaelstn@gmail.com>
2022-03-08 17:22:44 -0300

    test: add monad either in student

--- __tests__/domain/entities/student.spec.ts
+++ __tests__/domain/entities/student.spec.ts
@@ -3,7 +3,22 @@ import { Student } from '@/domain/entities';
 describe('Student domain Entity', () => {
   it('should be create student with valid name', () => {
     const name = 'Student 1';
-    const student = Student.create({ name }) as Student;
-    expect(student.props.name).toEqual(name);
+    const student = Student.create({ name }).value as Student;
+    expect(student.name.value).toEqual(name);
+  });
+  it('should be not create student with min invalid length name', () => {
+    const invalidName = 'S   ';
+
+    const error = Student.create({ name: invalidName }).value as Error;
+    expect(error.name).toEqual('InvalidNameError');
+    expect(error.message).toEqual(`Invalid name: ${invalidName}.`);
+  });
+
+  it('should be not create student with max invalid length name', () => {
+    const invalidName = 'S'.repeat(257);
+
+    const error = Student.create({ name: invalidName }).value as Error;
+    expect(error.name).toEqual('InvalidNameError');
+    expect(error.message).toEqual(`Invalid name: ${invalidName}.`);
   });
 });
