5e44efd6bec391f6e8318486abe2335ad2f45297
santanarscs <raphaelstn@gmail.com>
2022-03-10 16:40:16 -0300

    feat: return elements in container class

--- src/domain/entities/container.ts
+++ src/domain/entities/container.ts
@@ -3,10 +3,14 @@ import { Element } from '.';
 import { ExistingElementError } from '../errors';
 
 export class Container<T extends Element> {
-  private readonly elements: T[] = [];
+  private readonly _elements: T[] = [];
 
   get numberOfElements(): number {
-    return this.elements.length;
+    return this._elements.length;
+  }
+
+  get elements(): T[] {
+    return this._elements;
   }
 
   add(element: T): Either<ExistingElementError, void> {
@@ -16,35 +20,35 @@ export class Container<T extends Element> {
   }
 
   private push(element: T): void {
-    this.elements.push(element);
+    this._elements.push(element);
   }
 
   includes(element: T): boolean {
-    return this.elements.find(p => p.equals(element) === true) !== undefined;
+    return this._elements.find(p => p.equals(element) === true) !== undefined;
   }
 
   move(element: T, to: number): void {
-    if (to > this.elements.length || to < 1) return;
+    if (to > this._elements.length || to < 1) return;
     const from = this.position(element) as number;
     this.moveInArray(from - 1, to - 1);
   }
 
   position(element: T): number | undefined {
-    const partInContainer = this.elements.find(p => p.equals(element));
+    const partInContainer = this._elements.find(p => p.equals(element));
     if (partInContainer === undefined) {
       return undefined;
     }
-    return this.elements.indexOf(partInContainer) + 1;
+    return this._elements.indexOf(partInContainer) + 1;
   }
 
   remove(element: T): void {
     if (!this.includes(element)) return;
     const positionInArray = this.position(element) as number;
-    this.elements.splice(positionInArray - 1, 1);
+    this._elements.splice(positionInArray - 1, 1);
   }
 
   moveInArray(from: number, to: number): void {
-    const element = this.elements.splice(from, 1)[0];
-    this.elements.splice(to, 0, element);
+    const element = this._elements.splice(from, 1)[0];
+    this._elements.splice(to, 0, element);
   }
 }
