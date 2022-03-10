d115f6d87eb28f93c1a05e6bd55f6d8be77a3442
santanarscs <raphaelstn@gmail.com>
2022-03-10 16:40:45 -0300

    feat: update teacher class

--- __tests__/domain/entities/teacher.spec.ts
+++ __tests__/domain/entities/teacher.spec.ts
@@ -1,27 +1,48 @@
 import { Teacher, Subject } from '@/domain/entities';
 
 describe('Teacher domain Entity', () => {
+  it('should be not create teacher with min invalid length name', () => {
+    const invalidName = 'T   ';
+
+    const error = Teacher.create({ name: invalidName }).value as Error;
+    expect(error.name).toEqual('InvalidNameError');
+    expect(error.message).toEqual(`Invalid name: ${invalidName}.`);
+  });
+
   it('should be able to create teacher with valid name', () => {
     const name = 'Teacher 1';
-    const teacher = Teacher.create({ name }) as Teacher;
-    expect(teacher.props.name).toEqual(name);
+    const teacher = Teacher.create({ name }).value as Teacher;
+    expect(teacher.name.value).toEqual(name);
   });
 
   it('should be able to enable a subject for a teacher', () => {
     const nameSubject = 'Subject';
     const nameTeacher = 'Teacher 1';
-    const subject = Subject.create({ name: nameSubject }) as Subject;
-    const teacher = Teacher.create({ name: nameTeacher }) as Teacher;
-    teacher.enableSubject(subject);
-    expect(teacher.props.subjects).toHaveLength(1);
+    const subject = Subject.create({ name: nameSubject }).value as Subject;
+    const teacher = Teacher.create({ name: nameTeacher }).value as Teacher;
+    teacher.add(subject);
+    expect(teacher.numberOfSubjects).toEqual(1);
   });
+
   it('should be able to enable many subjects for a teacher', () => {
     const nameTeacher = 'Teacher 1';
-    const subject1 = Subject.create({ name: 'Subject 1' }) as Subject;
-    const subject2 = Subject.create({ name: 'Subject 2' }) as Subject;
-    const teacher = Teacher.create({ name: nameTeacher }) as Teacher;
-    teacher.enableSubject(subject1);
-    teacher.enableSubject(subject2);
-    expect(teacher.props.subjects).toHaveLength(2);
+    const subject1 = Subject.create({ name: 'Subject 1' }).value as Subject;
+    const subject2 = Subject.create({ name: 'Subject 2' }).value as Subject;
+    const teacher = Teacher.create({ name: nameTeacher }).value as Teacher;
+    teacher.add(subject1);
+    teacher.add(subject2);
+    expect(teacher.numberOfSubjects).toEqual(2);
+  });
+
+  it('should be able to remove subject', () => {
+    const nameTeacher = 'Teacher 1';
+    const subject1 = Subject.create({ name: 'Subject 1' }).value as Subject;
+    const subject2 = Subject.create({ name: 'Subject 2' }).value as Subject;
+    const teacher = Teacher.create({ name: nameTeacher }).value as Teacher;
+    teacher.add(subject1);
+    teacher.add(subject2);
+
+    teacher.remove(subject2);
+    expect(teacher.numberOfSubjects).toEqual(1);
   });
 });
--- src/domain/entities/teacher.ts
+++ src/domain/entities/teacher.ts
@@ -1,25 +1,65 @@
-import { Entity } from '@/core/domain/entities';
-import { Subject } from '.';
+import { Either, left, right } from '@/core/domain/entities';
+import { Container, Subject } from '.';
+import { ExistingElementError, InvalidNameError } from '../errors';
+import { Name } from './name';
 
-type TeacherProps = {
+interface ICreateTeacherData {
   name: string;
   subjects?: Subject[];
-};
-export class Teacher extends Entity<TeacherProps> {
-  private constructor(props: TeacherProps, id?: string) {
-    super(props, id);
+}
+export class Teacher {
+  private readonly _subjects: Container<Subject> = new Container<Subject>();
+
+  private constructor(private readonly _name: Name) {}
+
+  get name() {
+    return this._name;
   }
 
-  static create({ name }: TeacherProps, id?: string): Teacher {
-    const teacher = new Teacher({ name }, id);
-    return teacher;
+  get subjects(): Subject[] {
+    return this._subjects.elements;
   }
 
-  public enableSubject(subject: Subject): void {
-    if (this.props.subjects) {
-      this.props.subjects.push(subject);
-    } else {
-      this.props.subjects = [subject];
+  get numberOfSubjects(): number {
+    return this._subjects.numberOfElements;
+  }
+
+  add(subject: Subject): Either<ExistingElementError, void> {
+    return this._subjects.add(subject);
+  }
+
+  includes(subject: Subject): boolean {
+    return this._subjects.includes(subject);
+  }
+
+  remove(subject: Subject): void {
+    this._subjects.remove(subject);
+  }
+
+  static create(data: ICreateTeacherData): Either<InvalidNameError, Teacher> {
+    const nameOrError = Name.create(data.name);
+
+    if (nameOrError.isLeft()) {
+      return left(nameOrError.value);
     }
+    const name = nameOrError.value as Name;
+
+    const teacher = new Teacher(name);
+
+    if (data.subjects) {
+      data.subjects.forEach(subject => {
+        teacher.add(subject);
+      });
+    }
+
+    return right(teacher);
+  }
+
+  validateAvailableToTeach(subject: Subject) {
+    if (!this.numberOfSubjects) {
+      return false;
+    }
+
+    return this.includes(subject);
   }
 }
